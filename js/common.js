cdd0a378d35e8b1adddb6bf0123db1744e92e306
Виктория Захарова <zaharova181114@gmail.com>
2024-03-06 00:55:15 +0200

    all

--- js/common.js
+++ js/common.js
@@ -1,59 +1,64 @@
 $('[name="phone"]').mask('+7 (999) 999-99-99');
 
-$('.product-card-slider').slick({
-	slidesToShow: 1,
-	dots: true,
-	arrows: false,
-	// autoplay: true,
-	autoplaySpeed: 2000,
-});
+// $('.product-card-slider').slick({
+// 	slidesToShow: 1,
+// 	dots: true,
+// 	arrows: false,
+// 	// autoplay: true,
+// 	autoplaySpeed: 2000,
+// });
 
-$('.exclusive-slider').slick({
+// $('.exclusive-slider').slick({
+// 	slidesToShow: 1,
+// 	appendArrows: '.exclusive-slider__nav',
+// 	variableWidth: true,
+// 	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
+// 	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+// 	responsive: [
+// 		{
+// 			breakpoint: 576,
+// 			settings: {
+// 				slidesToShow: 1,
+// 				variableWidth: false
+// 			}
+// 		}
+// 	]
+// });
+
+$('.sales-slider').slick({
 	slidesToShow: 1,
-	appendArrows: '.exclusive-slider__nav',
 	variableWidth: true,
 	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
 	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+	appendArrows: '.sales-slider__nav',
 	responsive: [
 		{
-			breakpoint: 576,
+			breakpoint: 992,
 			settings: {
-				slidesToShow: 1,
-				variableWidth: false
-			}
-		}
-	]
-});
-
-$('.advantages-slider').slick({
-	slidesToShow: 4,
-	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
-	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
-	responsive: [
-		{
-			breakpoint: 1200,
-			settings: {
-				slidesToShow: 3,
-			}
-		},
-		{
-			breakpoint: 768,
-			settings: {
-				slidesToShow: 2,
+				variableWidth: true,
+				// autoplay: true,
+				arrows: false
 			}
 		},
 		{
 			breakpoint: 576,
 			settings: {
 				slidesToShow: 1,
+				variableWidth: true,
+				autoplay: true,
+				arrows: false
 			}
 		}
 	]
 });
 
-// $('[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
-// 	$('.slick-slider').slick('setPosition');
-// });
+let homeSlider = $('.sales-slider');
+
+$('.counter-slide__default').text("/" + homeSlider.slick("getSlick").slideCount);
+
+homeSlider.on('afterChange', function (event, slick, currentSlide) {
+	$(".counter-slide__cp").text(currentSlide < 10 ? `${currentSlide + 1}` : currentSlide + 1);
+});
 
 
 // password view
